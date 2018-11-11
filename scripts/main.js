5aa981a2a310d8a11425e7df91eb820b9cd19a93
bakaldina <bakaldina.e@gmail.com>
2018-11-12 00:53:35 +0300

    too many changes

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.headmenu-slider').slick({
+        dots: true,
+    });
+
+    let currentSlide = $('.headmenu-slider').slick('slickCurrentSlide') + 1;
+    let slideCount = $(".headmenu-slider").slick("getSlick").slideCount;
+    $('.currentSlide').html(currentSlide);
+    $('.slideCount').html(slideCount);
+
+    $(".headmenu-slider").on("afterChange", function (event, slick, currentSlide, nextSlide) {
+        $(".currentSlide").text(currentSlide + 1);
+    });
+
+    $('.calendar-modal').click(function () {
+        $('.modal').addClass('open')
+    });
+});
