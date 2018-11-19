70cf362361ae0295398b8a964539ce729e8f0291
Екатерина Бакалдина <e.bakaldina@city-mobil.ru>
2018-11-19 12:24:48 +0300

    edit

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         $('.slider-init').slick({
             dots: true,
             autoplay:  true,
-            autoplaySpeed: 3000,
+            autoplaySpeed: 2000,
             pauseOnHover: true
         });
         let currentSlide = $('.slider-init').slick('slickCurrentSlide') + 1;
