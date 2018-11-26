155156be609994f01e5c1052be4e44038a259192
Екатерина Бакалдина <e.bakaldina@city-mobil.ru>
2018-11-26 09:02:18 +0300

    height

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     if ($('.slider-init').length > 0) {
         $('.slider-init').slick({
             dots: true,
-            // autoplay:  true,
+            autoplay:  true,
             autoplaySpeed: 2000,
             pauseOnHover: true
         });
@@ -74,4 +74,17 @@ $(document).ready(function () {
 
     // для иконок
     feather.replace();
+
 });
+
+ // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+ let vh = window.innerHeight * 0.01;
+ // Then we set the value in the --vh custom property to the root of the document
+ document.documentElement.style.setProperty('--vh', `${vh}px`);g
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
